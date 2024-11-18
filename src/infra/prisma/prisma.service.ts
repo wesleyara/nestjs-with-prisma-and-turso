a6e47dad95a67d7557abc3e839bc81fa3ae72fa6
wesleyara <wes1gom@gmail.com>
2024-11-18 18:58:48 -0300

    feat: implement tursodb integration

--- src/infra/prisma/prisma.service.ts
+++ src/infra/prisma/prisma.service.ts
@@ -1,8 +1,20 @@
+import { createClient } from '@libsql/client/.';
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const libsql = createClient({
+      url: process.env.TURSO_DATABASE_URL,
+      authToken: process.env.TURSO_AUTH_TOKEN,
+    });
+
+    const adapter = new PrismaLibSQL(libsql);
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
