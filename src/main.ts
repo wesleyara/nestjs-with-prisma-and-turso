3bb20f07ec8376bf95bb6db1709f6a0b3c745ebe
wesleyara <wes1gom@gmail.com>
2024-11-18 18:52:52 -0300

    feat: set api prefix and version

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('/api');
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
+
   await app.listen(3000);
 }
 bootstrap();
